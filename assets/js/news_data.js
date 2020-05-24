f7bb08124a69beee935799557c72223af77e6686
alexiahsu <hsu.enyang@gmail.com>
2020-05-24 22:26:40 +0800

    complete news api

--- assets/js/news_data.js
+++ assets/js/news_data.js
@@ -1,25 +1,19 @@
 // define variables
 var $covidNews = $('#covidNews') 
-var $newsTitle = $('#newsTitle')
-var $newsUrl = $('#newsUrl')
-var $covidCountryNews = $('#covidCountryNews')
 
 
  // clear data
 function clearNewsData() {
     $covidNews.html('');
-    $newsTotalResults.html('');
 }
 
  // main news api
 function getCovidNews() {
-    covidNewsUrl = ('https://newsapi.org/v2/top-headlines?q=covid&sortBy=popularity&apiKey=810151c410ef4086baac829ca401a0ce');
+    covidNewsUrl = ('https://newsapi.org/v2/top-headlines?country=sg&q=covid&apiKey=810151c410ef4086baac829ca401a0ce');
     $.getJSON(covidNewsUrl, function(newsData) {
         console.log('Global covid news: ');
         console.log(newsData);
-        if (newsData.articles.length > 0) {
-            var newsTotalResults = 0;
-            
+        if (newsData.articles.length > 0) {            
             for (i=0; i < 3; i++) {
                 var newsTitle = newsData.articles[i].title;
                 var newsUrl = newsData.articles[i].url;
@@ -30,42 +24,11 @@ function getCovidNews() {
             }
         }
         else {
-            $covidNews.append('No results found');
+            $covidNews.append('Global news not available');
         }
     });
 };
 
-// country list
-function getCovidCountryNews() {
-    covidCountryNewsUrl = ('https://newsapi.org/v2/top-headlines?country=' + countrycode + '&q=covid&sortBy=popularity&apiKey=810151c410ef4086baac829ca401a0ce');
-    $.getJSON(covidCountryNewsUrl, function(countryNews){
-        console.log('Covid news by country: ')
-        console.log(countryNews);
-        if (countryNews.articles.length > 0) {
-            var newsTotalResults = 0;
-            
-            for (i=0; i < 3; i++) {
-                var countryNewsTitle = countryNews.articles[i].title;
-                var countryNewsUrl = countryNews.articles[i].url;
-
-                $covidCountryNews.append(
-                    '<li><a href="' + countryNewsUrl + '">' + countryNewsTitle + '</a></li><br/>'
-                );
-            }
-        }
-        else {
-            $covidCountryNews.append('No results found');
-        }
-    });
-}
-
-//country look-up
-$("searchCountries").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#countryNews").filter(function() {
-        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-});
 
 // reloading data
 function reloadNewsData() {
@@ -76,4 +39,4 @@ function reloadNewsData() {
 //load data on page load
 $(document).ready(function() {
     getCovidNews();
-});
+})
