8a8e5b3cfb0da93e3be46541153ea9e08f6a10cd
alexiahsu <hsu.enyang@gmail.com>
2020-05-25 15:50:09 +0800

    finalised news api

--- assets/js/news_data.js
+++ assets/js/news_data.js
@@ -1,30 +1,54 @@
 // define variables
-var $covidNews = $('#covidNews') 
+var $covidSgNews = $('#covidSgNews') 
+var $covidGlobalNews = $('#covidGlobalNews')
 
 
  // clear data
 function clearNewsData() {
-    $covidNews.html('');
+    $covidSgNews.html('');
+    $covidGlobalNews.html('');
 }
 
- // main news api
-function getCovidNews() {
-    covidNewsUrl = ('https://newsapi.org/v2/top-headlines?country=sg&q=covid&apiKey=810151c410ef4086baac829ca401a0ce');
+ // local news api
+function getSgCovidNews() {
+    covidNewsUrl = ('https://newsapi.org/v2/top-headlines?country=sg&sortBy=publishedAt&q=covid&apiKey=810151c410ef4086baac829ca401a0ce');
     $.getJSON(covidNewsUrl, function(newsData) {
-        console.log('Global covid news: ');
+        console.log('Local covid news: ');
         console.log(newsData);
         if (newsData.articles.length > 0) {            
             for (i=0; i < 3; i++) {
                 var newsTitle = newsData.articles[i].title;
                 var newsUrl = newsData.articles[i].url;
 
-                $covidNews.append(
+                $covidSgNews.append(
                     '<li><a href="' + newsUrl + '">' + newsTitle + '</a></li><br/>'
                 );
             }
         }
         else {
-            $covidNews.append('Global news not available');
+            $covidSgNews.append('Local news not available');
+        }
+    });
+};
+
+// global news api
+ function getGlobalCovidNews() {
+    covidGlobalNewsUrl = ('https://newsapi.org/v2/everything?q=covid&language=en&apiKey=810151c410ef4086baac829ca401a0ce');
+    $.getJSON(covidGlobalNewsUrl, function(newsGlobalData) {
+        console.log('Global covid news: ');
+        console.log(newsGlobalData);
+        if (newsGlobalData.articles.length > 0) {            
+            for (i=0; i < 3; i++) {
+                var newsTitleGlobal = newsGlobalData.articles[i].title;
+                var newsUrlGlobal = newsGlobalData.articles[i].url;
+
+                $covidGlobalNews.append(
+                    '<li><a href="' + newsUrlGlobal + '">' + newsTitleGlobal + '</a></li><br/>'
+                );
+            }
+        }
+        else {
+            $covidGlobalNews.append('Lcao news not available');
         }
     });
 };
@@ -33,10 +57,12 @@ function getCovidNews() {
 // reloading data
 function reloadNewsData() {
     clearNewsData();
-    getCovidNews();
+    getSgCovidNews();
+    getGlobalCovidNews();
 };
 
 //load data on page load
 $(document).ready(function() {
-    getCovidNews();
+    getSgCovidNews();
+    getGlobalCovidNews();
 })
